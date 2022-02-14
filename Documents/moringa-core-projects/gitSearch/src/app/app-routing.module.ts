35f8b02fe64602e3216b45930a8c5d2c7a469651
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-02-14 19:48:21 +0300

    add the router-otlet

--- Documents/moringa-core-projects/gitSearch/src/app/app-routing.module.ts
+++ Documents/moringa-core-projects/gitSearch/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profile', component: ProfileComponent},
+  { path: 'search', component: SearchComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
